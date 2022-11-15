65d9c5d270528779d4cef6e828065e3ae54d47ba
Вадим Клементьев <klementevv@quantumart.ru>
2022-11-15 11:21:01 +0300

    Refactoring

--- src/test/java/pochtabank/FeedbackTest.java
+++ src/test/java/pochtabank/FeedbackTest.java
@@ -1,8 +1,9 @@
 package pochtabank;
 
-import org.junit.Assert;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.time.Duration;
@@ -29,14 +31,12 @@ public class FeedbackTest {
     @DataProvider(name = "sms")
     public static Object[][] dbData() {
         return new Object[][]{
-                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", "Хьюстон, у вас проблемы", true},
-                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", null, true},
-                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", null, false},
-                {null, "9775676767", "testcase@mail.ru", null, true},
-                {"Клементьев Вадим Владимирович", null, "testcase@mail.ru", null, true},
-                {"Клементьев Вадим Владимирович", "9775676767", null, null, true},
-//                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", null, true},
-//                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", null, true},
+                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", "Хьюстон, у вас проблемы", true, "с заполнением всех полей валидными данными"},
+                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", null, true, "с заполнением только обязательных полей валидными данными"},
+                {"Клементьев Вадим Владимирович", "9775676767", "testcase@mail.ru", null, false, "Проверка обязательности поля \"Даю согласие на обработку  персональных данных\""},
+                {null, null, null, null, true, "Отправка \"Формы обратной связи\" с пустыми полями"},
+                {null, "9775676767", "testcase@mail.ru", null, true, "Проверка реакции системы при вводе невалидных данных в поле \"ФИО\""},
+                {"Клементьев Вадим Владимирович", "9775676767", "", null, true, "Проверка реакции системы при вводе невалидных данных в поле \"E-mail\""},
         };
     }
 
@@ -45,7 +45,8 @@ public class FeedbackTest {
         System.setProperty("webdriver.chrome.driver", "E:\\Projects\\Autotest\\ChromeDriver\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         //options.addArguments("--headless");
-        //options.addArguments("start-maximized");
+        options.addArguments("start-maximized");
+        options.addArguments("--disable-notifications");
         chrome = new ChromeDriver(options);
         chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(chrome, Duration.ofSeconds(10));
@@ -54,14 +55,10 @@ public class FeedbackTest {
 
     @Parameters({"sms"})
     @Test(dataProvider = "sms")
+    @Description(value = "Проверка работы \"Формы обратной связи\". {condition}")
     public void testNoSms(String fio, String phone, String mail, String comment, Boolean checkAgreement) {
-        testIndex++;
-        System.out.println("Test " + testIndex);
 
-//        try {
-//            Alert alert = wait.until(alertIsPresent());
-//            alert.dismiss();
-//        }catch (TimeoutException tex){}
+        System.out.println("Test " + (testIndex + 1));
 
         Actions act = new Actions(chrome);
         if (fio != null && fio.length() > 0)
@@ -69,7 +66,8 @@ public class FeedbackTest {
         if (phone != null && phone.length() > 0)
             try {
                 //act.moveToElement(elPhone).click().build().perform();
-                wait.until(ExpectedConditions.elementToBeClickable(elPhone)).click();
+                wait.until(ExpectedConditions.elementToBeClickable(elPhone));
+                act.moveToElement(elPhone).click().build().perform();
                 Thread.sleep(100);
                 for (char num : phone.toCharArray()) {
                     Thread.sleep(50);
@@ -77,28 +75,35 @@ public class FeedbackTest {
                 }
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            } catch (ElementClickInterceptedException ie) {
+                System.out.println(ie.getLocalizedMessage());
             }
         if (mail != null && mail.length() > 0)
             elMail.sendKeys(mail);
         if (comment != null && comment.length() > 0)
             elComment.sendKeys(comment);
-        if (!elAgreement.isSelected() && checkAgreement) {
-            act.moveToElement(elAgreement).click().build().perform();
-//            elAgreement.click();
-        }
         try {
+            if (checkAgreement) {
+                Thread.sleep(100);
+                wait.until(ExpectedConditions.elementToBeClickable(elAgreement));
+                act.moveToElement(elAgreement).click().build().perform();
+            }
             Thread.sleep(100);
-            wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+            wait.until(ExpectedConditions.elementToBeClickable(submitBtn));
             act.moveToElement(submitBtn).click().build().perform();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        } catch (ElementClickInterceptedException ie) {
+            System.out.println(ie.getLocalizedMessage());
         }
     }
 
     @BeforeMethod
+    @Step()
     private void openDriver() {
         try {
             chrome.get(URL);
+            //String window = chrome.getWindowHandle();
             Thread.sleep(2000);
             elFio = chrome.findElement(By.name("fio"));
             elPhone = chrome.findElement(By.name("phone"));
@@ -112,37 +117,45 @@ public class FeedbackTest {
     }
 
     @AfterMethod
+    @Step()
     private void checkResult() {
         try {
-            Object obj = wait.until(ExpectedConditions.jsReturnsValue("return document.querySelector('div.style_modalWrapper__UG622') != null"));
-            Assert.assertTrue(testIndex < 3 && (boolean) obj);
-            if ((boolean) obj)
-                System.out.println("Test pass");
-            else
-                System.out.println("Test fail");
-        } catch (TimeoutException ex) {
+            Thread.sleep(100);
             List<WebElement> errors = chrome.findElements(By.cssSelector("div[class^='style_error_']"));
-
-            switch (testIndex) {
-                case 3:
-                case 4:
-                case 5:
-                    Assert.assertEquals(errors.get(0).getText(), "Поле обязательно для заполнения");
-                    break;
-                case 6:
-                    Assert.assertEquals(errors.get(0).getText(), "Заполните поле E-mail");
-                    break;
-            }
-            if (errors.size() == 1)
+            if (errors.size() == 0) {
+                wait.until(ExpectedConditions.jsReturnsValue("return document.querySelector('div.style_modalWrapper__UG622')"));
+                Assert.assertTrue(testIndex < 3, dbData()[testIndex][5].toString());
                 System.out.println("Test pass");
-            else
-                System.out.println("Test fail");
+            } else {
+                checkErrors(errors);
+            }
+        } catch (InterruptedException ex) {
+            System.out.println("Test fail");
         }
+        testIndex++;
+    }
 
+    private void checkErrors(List<WebElement> errors) {
+        switch (testIndex) {
+            case 3:
+                Assert.assertEquals(errors.size(), 3, dbData()[testIndex][5].toString());
+                break;
+            case 2:
+            case 4:
+                Assert.assertEquals(errors.size(), 1, dbData()[testIndex][5].toString());
+                Assert.assertEquals(errors.get(0).getText(), "Поле обязательно для заполнения");
+                break;
+            case 5:
+                Assert.assertEquals(errors.size(), 1, dbData()[testIndex][5].toString());
+                Assert.assertEquals(errors.get(0).getText(), "Заполните поле E-mail");
+                break;
+        }
+        System.out.println("Test pass");
     }
 
     @AfterTest
     private void closeDriver() {
-        chrome.close();
+        if (chrome != null)
+            chrome.quit();
     }
 }
